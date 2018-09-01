4dbfca16a15057a430b90f899b31567eb27f38a8
Łukasz Ślusarczyk <lukasz.slusarczyk@gmail.com>
2018-09-01 15:00:19 +0200

    changed rfid to be bigint

--- db/migrate/002_create_rfids.rb
+++ db/migrate/002_create_rfids.rb
@@ -2,7 +2,7 @@ class CreateRfids < ActiveRecord::Migration[5.2]
   def change
     create_table :rfids do |t|
       t.references :user, foreign_key: true
-      t.integer :number
+      t.integer :number, :limit => 8
 
       t.timestamps
     end
